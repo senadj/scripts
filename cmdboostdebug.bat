bf8aab2628fb887aae625415ee6141e2613e86b1
senadj <senad.jahic@gmx.com>
2018-02-19 19:23:17 +0100

    bood

--- cmdboostdebug.bat
+++ cmdboostdebug.bat
@@ -3,7 +3,10 @@ rem cmd started from MSYS has problems running subsequent commands after bootstr
 @echo on
 
 bootstrap mingw && ^
-b2 -a toolset=gcc cxxflags="-Og -std=gnu++0x -static -static-libgcc -static-libstdc++ -Wno-deprecated-declarations -Wno-unused-variable -Wno-unknown-pragmas" ^
+b2 -a toolset=gcc cxxflags="-Og -std=gnu++0x -Wno-deprecated-declarations -Wno-unused-variable -Wno-unknown-pragmas" ^
 --with-chrono --with-filesystem --with-regex --with-program_options --with-system --with-thread --with-date_time ^
---link=static --runtime-link=static linkflags="-static -static-libgcc -static-libstdc++" threading=multi variant=debug stage && ^
-exit
+--link=static --runtime-link=static linkflags="-static -static-libgcc -static-libstdc++ -s" threading=multi variant=debug --prefix="%YBOO%" install && ^
+cd "%YBOO%/include" && ^
+move "INCLUDESUBDIR/boost" . && ^
+cd "%YBOO%/lib" && ^
+for /f "tokens=1,2,3,4,5 delims=-." %%i in ('dir /B "*.a"') DO @echo off && ren "%%i-%%j-%%k-%%l.%%m" "%%i.%%m" && @echo on && echo "Boost build done. Please close window or type exit."
